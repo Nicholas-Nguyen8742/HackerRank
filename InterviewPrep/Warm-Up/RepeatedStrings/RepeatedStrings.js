7be7ce7c5c70e3cff3e779d102ee3ee3c0a854b0
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-07-11 16:13:03 -0400

    Repeated String: Passed all test cases

--- InterviewPrep/Warm-Up/RepeatedStrings/RepeatedStrings.js
+++ InterviewPrep/Warm-Up/RepeatedStrings/RepeatedStrings.js
@@ -7,7 +7,34 @@
  *  2. LONG_INTEGER n
  */
 
+// Start) s = string to repeat, n = number of characters to look at
+    // Get length of string = length
+    // Find frequency of 'a'
+        // Split into array by character to access
+        // Filter then count new array
+    // if (length < n)  
+        // Repeat string until 
+
+// End) Frequency of the 'a' within n
+
 function repeatedString(s, n) {
     // Write your code here
+    // 1) Length of String
+    let length = s.length;
 
+    // 2) Count of "a" in string
+    let count = (s.split('a').length) - 1;
+
+    // 3) Max occurences of "a" in whole number
+    let maxWholeNum = Math.floor(n / length);
+    
+    // 4) count of total so far of "a"
+    let total = count * maxWholeNum;
+    
+    // 5) Slice the string from start to remainder
+    // 6) from there count occurences of 
+    // 7) Add to total above and return
+    total += (s.slice(0, n % length).split("a").length - 1);
+    return total;
 }
+
