087cc6537f692257b497afed5d99606accf614e2
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-07-11 14:19:27 -0400

    Sales By Match: Passed all test cases

--- InterviewPrep/Warm-Up/SalesByMatch/SalesByMatch.js
+++ InterviewPrep/Warm-Up/SalesByMatch/SalesByMatch.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -31,26 +31,45 @@ function readLine() {
  *  2. INTEGER_ARRAY ar
  */
 
+// Initialize count to 0 to count matches
+// Find unique values
+// Filter out all values of array matching unique value 
+// Use count divided by two, rounding down and add to count
+
 function sockMerchant(n, ar) {
-    // Write your code here
-    // Initialize empty array to slice matching socks
-        // Array count divided by two is the number of pairs
-    // Sort array in ascending order 
-        // Go through array
-        // 
-     
+  // Write your code here
+  // Initialize count of matches
+  let count = 0;
+
+  // Unique values for comparison
+  const unique = ar.filter((value, index, self) => self.indexOf(value) === index);
+
+  // Go through array and filter out all values matching key
+  unique.forEach((value) => {
+    // Filter out all index values with matching unique value of comparison
+    const filtered = ar.filter((element) => element == value);
+    // Round down the count divided by 2
+    const pairs = Math.floor(filtered.length / 2);
+    // Add to count
+    count += pairs;
+  });
+
+  return count;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine().trim(), 10);
+  const n = parseInt(readLine().trim(), 10);
 
-    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+  const ar = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arTemp) => parseInt(arTemp, 10));
 
-    const result = sockMerchant(n, ar);
+  const result = sockMerchant(n, ar);
 
-    ws.write(result + '\n');
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
