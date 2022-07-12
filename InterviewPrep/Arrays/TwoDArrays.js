7dfc8f0e9f463117c821fb1e6bac09f6456b894e
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-07-12 07:29:30 -0400

    Initial setup for 2D Arrays

---
+++ InterviewPrep/Arrays/TwoDArrays.js
@@ -0,0 +1,11 @@
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function hourglassSum(arr) {
+    // Write your code here
+
+}
